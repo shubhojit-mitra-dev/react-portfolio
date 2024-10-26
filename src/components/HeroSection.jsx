f231e37a0ecdc1e375f20fe404e464cdac94a6f3
Shubhojit-Mitra <shubhojit.120225@stu.upes.ac.in>
2024-10-26 14:15:15 +0530

    UPDATE resume link

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,6 +7,8 @@ import CustomButton from './ui/CustomButton';
 const HeroSection = () => {
   const isSmallScreen = useMediaQuery({ query: '(max-width: 640px)' });
 
+  const resumeLink = '/react-portfolio/resume.pdf';
+
   return (
     <>
       <div className='relative flex justify-center items-center p-4' style={{ height: 'calc(100vh - 4rem)' }}>
@@ -17,7 +19,7 @@ const HeroSection = () => {
             Front-End Developer dedicated to creating seamless, user-centered web experiences through clean code, intuitive design, and a passion for innovation.
           </p>
           <div className='mt-10 flex flex-wrap justify-center space-y-0 space-x-5 sm:space-y-0 sm:space-x-10'>
-            <CustomButton content="Download CV" icon={downloadIcon} variant="blue" size={isSmallScreen ? 'm' : 'xl'} link="%PUBLIC_URL%/resume.pdf" />
+            <CustomButton content="Download CV" icon={downloadIcon} variant="blue" size={isSmallScreen ? 'm' : 'xl'} link={resumeLink} />
             <CustomButton content="Contact Me" icon={contactIcon} variant="white" size={isSmallScreen ? 'm' : 'xl'} link="#contact" />
           </div>
         </div>
