fc8cc8db47f0ea8e74ef37d52caf2ee9d693eb08
Shubhojit-Mitra <shubhojit.120225@stu.upes.ac.in>
2024-10-27 13:32:08 +0530

    ADD Social Media Links

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,6 +3,9 @@ import { useMediaQuery } from 'react-responsive';
 import downloadIcon from '../assets/download.svg';
 import contactIcon from '../assets/contact.svg';
 import CustomButton from './ui/CustomButton';
+import githubIcon from '../assets/github.svg'
+import linkedinIcon from '../assets/linkedin.svg'
+import instaIcon from '../assets/instagram.svg'
 
 const HeroSection = () => {
   const isSmallScreen = useMediaQuery({ query: '(max-width: 640px)' });
@@ -21,6 +24,17 @@ const HeroSection = () => {
           <div className='mt-10 flex flex-wrap justify-center space-y-0 space-x-5 sm:space-y-0 sm:space-x-10'>
             <CustomButton content="Download CV" icon={downloadIcon} variant="blue" size={isSmallScreen ? 'm' : 'xl'} link={resumeLink} />
             <CustomButton content="Contact Me" icon={contactIcon} variant="white" size={isSmallScreen ? 'm' : 'xl'} link="#contact" />
+          </div>
+          <div className='flex justify-center mt-10 gap-10'>
+          <a href="https://linkedin.com/in/shubhojit-mitra-dev" target="_blank">
+              <img src={linkedinIcon} alt="LinkedIn" className='w-12' />
+            </a>
+            <a href="https://github.com/shubhojit-mitra-dev" target="_blank">
+              <img src={githubIcon} alt="GitHub" className='w-12' />
+            </a>
+            <a href="https://instagram.com/m.shubhojit_2005" target="_blank">
+              <img src={instaIcon} alt="Instagram" className='w-12' />
+            </a>
           </div>
         </div>
       </div>
